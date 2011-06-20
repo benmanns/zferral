f595a4cf090d476083e5e8b4c457a53194dccfd7
Benjamin Manns <benmanns@gmail.com>
2011-06-20 10:37:52 -0400

    Explicitly require Zferral::Resource before each resource.
    Some systems require a specific resource, like Campaign, before the parent class when using the Dir glob, causing an exception.

--- lib/zferral/resources/campaign.rb
+++ lib/zferral/resources/campaign.rb
@@ -1,3 +1,5 @@
+require File.join(File.dirname(__FILE__), '..', 'resource')
+
 module Zferral
   # A Zferral Campaign resource, that behaves like a RESTful Resource
   #
--- lib/zferral/resources/event.rb
+++ lib/zferral/resources/event.rb
@@ -1,3 +1,5 @@
+require File.join(File.dirname(__FILE__), '..', 'resource')
+
 module Zferral
   # A Zferral Event Resource
   #
