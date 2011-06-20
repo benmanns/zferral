75b119b740afa6a1e05c052705840c76069a9e20
Benjamin Manns <benmanns@gmail.com>
2011-06-20 12:07:06 -0400

    Clean up Zferral spec.

--- spec/zferral_spec.rb
+++ spec/zferral_spec.rb
@@ -1,6 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Zferral" do
+  subject { Zferral.new(:subdomain => subdomain, :api_token => api_token) }
+
   describe ".new" do
     it "creates a new instance of Zferral::Client" do
       Zferral.new.should be_a(Zferral::Client)
@@ -8,18 +10,16 @@ describe "Zferral" do
   end
   
   describe "#campaign" do
-    it "provides access to the Campaign class with a linkage to the client" do
-      zferral = Zferral.new(:subdomain => subdomain, :api_token => api_token)
-      zferral.campaign.should == Zferral::Campaign
-      zferral.campaign.client.should == zferral
+    specify { subject.campaign.should == Zferral::Campaign }
+    it 'should set client to reference self' do
+      subject.campaign.client.should == subject
     end
   end
 
   describe "#event" do
-    it "provides access to the Event class with a linkage to the client" do
-      zferral = Zferral.new(:subdomain => subdomain, :api_token => api_token)
-      zferral.event.should == Zferral::Event
-      zferral.event.client.should == zferral
+    specify { subject.event.should == Zferral::Event }
+    it 'should set client to reference self' do
+      subject.event.client.should == subject
     end
   end
 end
