9db8e38348f0b529f87c0a4365c41e92ee5d429b
Benjamin Manns <benmanns@gmail.com>
2011-06-24 14:28:00 -0400

    Use subject instead of let for Campaign spec.

--- spec/campaign_spec.rb
+++ spec/campaign_spec.rb
@@ -3,36 +3,36 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe Zferral::Campaign do
   use_vcr_cassette "campaigns"
 
-  let(:campaign) { Zferral.new(:subdomain => subdomain, :api_token => api_token).campaign }
+  subject { Zferral.new(:subdomain => subdomain, :api_token => api_token).campaign }
 
   describe "#list" do
     it "returns an array of your campaigns" do
-      campaign.list.should be_an_array_of_campaigns
+      subject.list.should be_an_array_of_campaigns
     end
     
     it "is aliased by #all" do
-      campaign.all.should == campaign.list
+      subject.all.should == subject.list
     end
     
     it "is aliased by #find(:all)" do
-      campaign.find(:all).should == campaign.list
+      subject.find(:all).should == subject.list
     end
   end
   
   describe "#fetch" do
     context "given a valid ID" do
       it "fetches and returns the campaign" do
-        campaign.fetch(1).should be_a_campaign
+        subject.fetch(1).should be_a_campaign
       end
     end
     context "given an invalid ID" do
       it "raises a ResourceNotFound exception" do
-        lambda { campaign.fetch(9999999) }.should raise_error(Zferral::ResourceNotFound, "campaign with id=9999999 was not found")
+        lambda { subject.fetch(9999999) }.should raise_error(Zferral::ResourceNotFound, "campaign with id=9999999 was not found")
       end
     end
     
     it "is aliased by #find(id)" do
-      campaign.find(1).should == campaign.fetch(1)
+      subject.find(1).should == subject.fetch(1)
     end
   end
 end
