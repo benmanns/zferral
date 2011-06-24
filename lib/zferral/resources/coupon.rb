6380d439853196fc209e2c9a6d9abec735873f31
Benjamin Manns <benmanns@gmail.com>
2011-06-24 14:40:19 -0400

    Added Coupon.get_by_code method.

--- lib/zferral/resources/coupon.rb
+++ lib/zferral/resources/coupon.rb
@@ -5,5 +5,9 @@ module Zferral
   #
   # http://zferral.com/api-docs/coupon
   class Coupon < Resource
+    def self.get_by_code code
+      response = get("/coupon/getByCode/#{code}.json").parsed_response
+      handle_fetch response
+    end
   end
 end
